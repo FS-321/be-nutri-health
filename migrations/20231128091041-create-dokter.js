266afc27786fa3d3b6e3f7dda52b5011dfc21389
AHanif09 <hanifahmad876@gmail.com>
2023-11-28 16:22:14 +0700

    membuat model user, dokter, dan poli

---
+++ migrations/20231128091041-create-dokter.js
@@ -0,0 +1,58 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Dokter', {
+      dokter_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      no_telp: {
+        type: Sequelize.STRING
+      },
+      spesialis: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      foto_dokter: {
+        type: Sequelize.STRING
+      },
+      poli_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'poliklinik',
+          key: 'poli_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Dokters');
+  }
+};
---
+++ models/dokter.js
@@ -0,0 +1,62 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Dokter extends Model {
+    static associate(models) {
+      Dokter.hasMany(models.JadwalPraktik, {
+        foreignKey: 'dokter_id',
+      });
+      Dokter.belongsTo(models.Poliklinik, {
+        foreignKey: 'poli_id',
+      });
+    }
+  }
+  Dokter.init({
+    dokter_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    no_telp: {
+      type: DataTypes.STRING
+      // allowNull true by default
+    },
+    spesialis: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    foto_dokter: {
+      type: DataTypes.STRING
+      // allowNull true by default
+    },
+    poli_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Poliklinik',
+        key: 'poli_id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Dokter',
+    tableName: 'dokter'
+  });
+  return Dokter;
+};

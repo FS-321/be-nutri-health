a4bf06d2efb33f181d8684c4b92a80a0ed8f5c1f
Habil <legendaryma30@gmail.com>
2023-11-29 08:54:46 +0700

    poliklinik controller

--- express/controller/dokter/dokter.js
+++ express/controller/dokter/dokter.js
@@ -4,28 +4,56 @@ const DataTypes = require('sequelize')
 const Dokter = require('../../../models/dokter')
 
 module.exports = {
-    getAll(req, res) {
+    async getAll(req, res) {
+        const page = req.query.page || 1
+        const pageSize = req.query.pageSize || 10
+        const offset = (page - 1) * pageSize
         try {
-            const dokter = Dokter.findAll()
-            if (!dokter) return res.status(404).send({ message: "doctor not found" })
+            const dokter = await Dokter.findAll({offset,limit:pageSize})
 
-            return dokter
+            return res.status(200).send(dokter)
 
         } catch (e) {
             return res.status(500).send({ message: "something happen when fetching doctor" })
         }
     },
 
-    getOne(req, res) {
+    async getOne(req, res) {
         const id = req.params
         try {
-            const dokter = Dokter.findOne({ where: { id } })
-            if (!dokter) return res.status(200).send({ message: "docter not found" })
+            const dokter = await Dokter.findOne({ where: { id } })
 
-            return dokter
+            return res.status(200).send(dokter)
         } catch (e) {
             return res.status(500).send({ message: "Something error when fetchind doctor" })
         }
-    }
+    },
 
+    async update(req, res) {
+        const data = req.body
+        const id = req.params.id
+        try {
+            const status = await Dokter.update(data, { where: id })
+
+            if (!status) return res.status(404).send({ message: "doctor not found" })
+
+            return res.status(200).send({ message: "update succesful" })
+
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when updating doctor" })
+        }
+    },
+
+    async deleteOne(req, res) {
+        const id = req.params.id
+        try {
+            const status = await Dokter.destroy({ where: { id } })
+
+            if (!status) return res.status(404).send({ message: "doctor is not found" })
+
+            return res.status(200).send({ message: "delete successful" })
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when deleting" })
+        }
+    }
 }
---
+++ express/controller/poliklinik/poliklinik.js
@@ -0,0 +1,59 @@
+const sequelize = require('../../sequelize-instance')
+const DataTypes = require('sequelize')
+const Poliklinik = require('../../../models/poliklinik')(sequelize, DataTypes)
+
+module.export = {
+    async getAll(req, res) {
+        const page = req.query.page || 1
+        const pageSize = req.query.pageSize || 10
+        const offset = (page - 1) * pageSize
+        try {
+            const poliklinik = await Poliklinik.findAll({ offset, limit: pageSize })
+
+            return res.status(200).send(poliklinik)
+        } catch (e) {
+            res.status(500).send({ message: "something happen when fetching poliklinik" })
+        }
+    },
+
+    async getOne(req, res) {
+        const id = req.params.id
+
+        try {
+            const poliklinik = await Poliklinik.findOne({ where: id })
+
+            return res.status(200).send(poliklinik)
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when fetching poliklinik" })
+        }
+    },
+
+    async update(req, res) {
+        const id = req.params.id
+        const data = req.body
+        try {
+            const status = await Poliklinik.update(data, { where: id })
+
+            return res.status(200).send(status)
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when updating poliklinik" })
+        }
+
+    },
+
+    async deleteOne(req, res) {
+        const id = req.params.id
+        try {
+            const status = await Poliklinik.destroy({ where: id })
+
+            if (!status) return res.status(404).send({ message: "poliklinik not found" })
+            return res.status(200).send({ message: "delete succseful" })
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when deleting poliklinik" })
+        }
+    }
+
+
+
+}
+

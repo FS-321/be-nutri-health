3b53114635c151a271cebf7ad7554385f609f571
AHanif09 <hanifahmad876@gmail.com>
2023-11-30 15:47:56 +0700

    membuat controller layanan, favorite, dan jadwal praktik

---
+++ express/controller/jadwal_praktik/jadwal_praktik.js
@@ -0,0 +1,59 @@
+const sequelize = require('../../sequelize-instance')
+const DataTypes = require('sequelize')
+
+const Jadwal_praktik = require('../../../models/jadwalPraktik')
+
+module.exports = {
+    async getAll(req, res) {
+        const page = req.query.page || 1
+        const pageSize = req.query.pageSize || 10
+        const offset = (page - 1) * pageSize
+        try {
+            const jadwal_praktik = await Jadwal_praktik.findAll({offset,limit:pageSize})
+
+            return res.status(200).send(jadwal_praktik)
+
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when fetching jadwal praktik" })
+        }
+    },
+
+    async getOne(req, res) {
+        const id = req.params
+        try {
+            const jadwal_praktik = await Jadwal_praktik.findOne({ where: { jadwal_id : id } })
+
+            return res.status(200).send(jadwal_praktik)
+        } catch (e) {
+            return res.status(500).send({ message: "Something error when fetchind jadwal praktik" })
+        }
+    },
+
+    async update(req, res) {
+        const data = req.body
+        const id = req.params.id
+        try {
+            const status = await Jadwal_praktik.update(data, { where: { jadwal_id : id } })
+
+            if (!status) return res.status(404).send({ message: "jadwal praktik not found" })
+
+            return res.status(200).send({ message: "update succesful" })
+
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when updating jadwal praktik" })
+        }
+    },
+
+    async deleteOne(req, res) {
+        const id = req.params.id
+        try {
+            const status = await Jadwal_praktik.destroy({ where: { jadwal_id : id } })
+
+            if (!status) return res.status(404).send({ message: "jadwal praktik is not found" })
+
+            return res.status(200).send({ message: "delete successful" })
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when deleting" })
+        }
+    }
+}
---
+++ express/controller/layanan/layanan.js
@@ -0,0 +1,59 @@
+const sequelize = require('../../sequelize-instance')
+const DataTypes = require('sequelize')
+
+const Layanan = require('../../../models/layanan')
+
+module.exports = {
+    async getAll(req, res) {
+        const page = req.query.page || 1
+        const pageSize = req.query.pageSize || 10
+        const offset = (page - 1) * pageSize
+        try {
+            const layanan = await Layanan.findAll({offset,limit:pageSize})
+
+            return res.status(200).send(layanan)
+
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when fetching layanan" })
+        }
+    },
+
+    async getOne(req, res) {
+        const id = req.params
+        try {
+            const layanan = await Layanan.findOne({ where: { layanan_id : id } })
+
+            return res.status(200).send(layanan)
+        } catch (e) {
+            return res.status(500).send({ message: "Something error when fetchind layanan" })
+        }
+    },
+
+    async update(req, res) {
+        const data = req.body
+        const id = req.params.id
+        try {
+            const status = await Layanan.update(data, { where: { layanan_id : id } })
+
+            if (!status) return res.status(404).send({ message: "layanan not found" })
+
+            return res.status(200).send({ message: "update succesful" })
+
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when updating layanan" })
+        }
+    },
+
+    async deleteOne(req, res) {
+        const id = req.params.id
+        try {
+            const status = await Layanan.destroy({ where: { layanan_id : id } })
+
+            if (!status) return res.status(404).send({ message: "layanan is not found" })
+
+            return res.status(200).send({ message: "delete successful" })
+        } catch (e) {
+            return res.status(500).send({ message: "something happen when deleting" })
+        }
+    }
+}

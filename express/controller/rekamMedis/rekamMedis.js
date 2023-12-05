735978d445ce2ba20494cc4a07ebfbb909a2af91
Habil <legendaryma30@gmail.com>
2023-12-05 21:52:38 +0700

    fix typo

--- express/controller/rekamMedis/rekamMedis.js
+++ express/controller/rekamMedis/rekamMedis.js
@@ -71,8 +71,8 @@ module.exports = {
     },
     async getAllbyUser(req, res) {
         const pasien_id = getDecodedToken(req, res)['user_id'].toString()
-        const page = req.query.pages || 1
-        const pageSize = req.query.limit || 10
+        const page = +req.query.pages || 1
+        const pageSize = +req.query.limit || 10
         const offset = (page - 1) * pageSize
         console.log('ini rekam oleh user', pasien_id, typeof pasien_id)
         try {

7a8649867c526c5e3d90514ddd1939220af4b292
Habil <legendaryma30@gmail.com>
2023-12-05 13:41:25 +0700

    fix cari makanan route

--- express/routing/routes/makanan.js
+++ express/routing/routes/makanan.js
@@ -5,7 +5,7 @@ const makananRoutesUser = express.Router()
 const makanan = require('../../controller/makanan/makanan')
 
 
-makananRoutes.get('/makanan/cari', makanan.search)
+makananRoutes.get('/cari/makanan', makanan.search)
 makananRoutes.get('/makanan', makanan.getAll)
 makananRoutes.get('/makanan/:id', makanan.getOne)
 
